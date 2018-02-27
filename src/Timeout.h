3bbfe43080f89e9ed226bfbfff4c6a13020df24c
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2018-02-27 23:51:47 +0300

    Add Timeout

---
+++ src/Timeout.h
@@ -0,0 +1,34 @@
+/*********************************************************************
+ * Class helps to run code after specified amount of time.
+ *
+ * Written by Valery Akhrymchuk
+*********************************************************************/
+
+#ifndef TIMEOUT_H
+#define TIMEOUT_H
+
+#include <Arduino.h>
+
+/**
+ * Create a single event that will be ready after timeout milliseconds.
+ */
+class Timeout {
+public:
+
+    bool isReady() const {
+        return millis() >= msReady;
+    }
+
+    void start(unsigned int timeout) {
+        msReady = millis() + timeout;
+    }
+
+    void startSec(unsigned int timeout) {
+        start(timeout * 1000);
+    }
+
+protected:
+    unsigned long msReady = 0;
+};
+
+#endif
