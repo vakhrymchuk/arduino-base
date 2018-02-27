b40ed963a5eb7f5488fc38740f33a29feef937c5
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2018-02-27 23:53:38 +0300

    Add Interval

---
+++ src/Interval.h
@@ -0,0 +1,45 @@
+/*********************************************************************
+ * Class helps to run code with specified period.
+ *
+ * Written by Valery Akhrymchuk
+*********************************************************************/
+
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include <Arduino.h>
+
+/**
+ * Create event that should be run periodically over interval.
+ */
+class Interval {
+public:
+
+    Interval(unsigned long interval) : interval(interval) { }
+
+    bool isReady() {
+        bool value = millis() >= msReady;
+        if (value) {
+            recalculate();
+        }
+        return value;
+    }
+
+    void recalculate() {
+        msReady += interval;
+    }
+
+    void startWithCurrentTime() {
+        msReady = millis() + interval;
+    }
+
+    void startWithCurrentTimeEnabled() {
+        msReady = millis();
+    }
+
+private:
+    const unsigned long interval;
+    unsigned long msReady = 0;
+};
+
+#endif
